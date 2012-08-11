f6470b037d6bbb06840fa928425c09f3f3626ff5
Ildar <nurildar9@gmail.com>
2012-08-12 00:12:46 +0400

    Anti-Fire and enter doors

--- urls.py
+++ urls.py
@@ -10,7 +10,11 @@ urlpatterns = patterns('',
     url(r'^prise-enter$', direct_to_template, {'template': 'prise_enter.html'}),
     url(r'^prise-parade$', direct_to_template, {'template': 'prise_parade.html'}),
     url(r'^prise-lattice$', direct_to_template, {'template': 'prise_lattice.html'}),
-    url(r'^color', direct_to_template, {'template': 'color.html'}),
+    url(r'^prise-anti-fire$', direct_to_template, {'template': 'prise_anti_fire.html'}),
+    url(r'^color$', direct_to_template, {'template': 'color.html'}),
+    url(r'^test$', direct_to_template, {'template': 'test.html'}),
+
+
 
 )
 
