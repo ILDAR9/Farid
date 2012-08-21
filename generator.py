16148888a474e728db9fd9681535e64c0131be0a
Ildar <nurildar9@gmail.com>
2012-08-21 17:12:20 +0400

    Version 1.2 corrected

--- generator.py
+++ generator.py
@@ -1,10 +1,8 @@
 file = open('panel.txt', 'w')
 for x in range(1,10):
-    file.write("""<a rel="doors" class="intdoor" href="../media/indoorVariants/panel/Pannels%s%s.jpg"><img
-    src="../media/indoorVariants/panel/min/Pannels%s%s.jpg"></a>\n""" %(0,x,0,x))
-for x in range(10,86):
-    file.write("""<a rel="doors" class="intdoor" href="../media/indoorVariants/panel/Pannels%s.jpg"><img
-    src="../media/indoorVariants/panel/min/Pannels%s.jpg"></a>\n""" %(x,x))
+    file.write("http://www.germes-doors.ru/images/palitra/small/0%d.jpg\n" %(x))
+for x in range(10,11):
+    file.write("http://www.germes-doors.ru/images/palitra/%d.jpg" %(x))
 file.close()
 
 
